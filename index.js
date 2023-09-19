57f86a0fabf0b97b72dd6db4e8d6ea62325236c4
Ian <gichaneian@gmail.com>
2023-09-19 11:52:05 +0300

    Arithmetic follow through

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1= 2;
+let num2= 31;
+let multiply = num1*num2;
+
+
+//random
+let random = Math.floor(Math.random()*10)
+
+//remainder
+let num3=34;
+let num4=5
+let mod = num3%num4
+
+//max in a set
+let max= Math.max(1,2,3,4,20)
